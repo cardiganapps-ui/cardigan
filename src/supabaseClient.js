bd0aaa1dfa9a54db4b6187d507a50a57341e83d1
cardiganapps-ui <cardigan.apps@gmail.com>
2026-04-07 22:40:14 -0600

    force vite 5

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://axyuqfkmifcaupwhzfuw.supabase.co'
+const supabaseKey = sb_publishable_fg2F4VcZ0qUk-d8DnIsP_A_jSKJ3xm2
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
